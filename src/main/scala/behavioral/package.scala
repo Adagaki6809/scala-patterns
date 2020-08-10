f1f3c570ae5b46a08c30011ca5f4672cad808dc0
Hitagi <include.ofstream.h@gmail.com>
2020-08-10 20:15:53 +0300

    added chain of responsibility, command, iterator, mediator

--- src/main/scala/behavioral/package.scala
+++ src/main/scala/behavioral/package.scala
@@ -1,5 +1,8 @@
 package object behavioral {
   def behavioralPatternDemo(): Unit = {
-
+    chainofresponsibility.chainofresponsibilityDemo()
+    command.commandDemo()
+    iterator.iteratorDemo()
+    mediator.mediatorDemo()
   }
 }
--- src/main/scala/structural/composite/TaskManager.scala
+++ src/main/scala/structural/composite/TaskManager.scala
@@ -2,16 +2,12 @@ package structural.composite
 
 class TaskManager(assignees: Array[Assignee]) {
   def performTask(task: String): Unit = {
-    try {
-      this.assignees.foreach(assignee => {
-        if (assignee.canHandleTask(task)) {
-          assignee.takeTask(task)
-          return
-        }
-      })
-    }
-    catch {
-      case e: Exception => println(e.getMessage + "Невозможно выполнить задание - пожалуйста,наймите больше людей.")
-    }
+    this.assignees.foreach(assignee => {
+      if (assignee.canHandleTask(task)) {
+        assignee.takeTask(task)
+        return
+      }
+    })
+    throw new Exception("Невозможно выполнить задание - пожалуйста,наймите больше людей.")
   }
 }
